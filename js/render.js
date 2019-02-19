33f1898a9f779fc329d9b34f4fcc106f6b80fd11
viktorkan <viktorkan9@gmail.com>
2019-02-19 19:50:21 +0200

    Рефакторинг кода

--- js/render.js
+++ js/render.js
@@ -8,13 +8,30 @@
 		var elementPin = pinTempleate.querySelector('.map__pin').cloneNode(true);
 		elementPin.style.left = element.location.x + 'px';
 		elementPin.style.top = element.location.y + 'px';
+		elementPin.querySelector('img').src = element.author.avatar;
 		map.appendChild(elementPin)
 	}
 
-	window.load(function(element) {
-	for (var i = 0; i < element.length; i++) {
-		renderPin(element[i]);
+
+	var cardTempleate = document.querySelector('#card');
+	var renderCard = function(element) {
+		var elementCard = cardTempleate.querySelector('.map__card').cloneNode(true);
+		elementCard.querySelector('.popup__avatar').src = element.author.avatar;
+		elementCard.querySelector('.popup__title').textContent = element.offer.title;
+		elementCard.querySelector('.popup__text--address').textContent = element.offer.address;
+
 	}
+
+	window.load(function(element) {
+		element.forEach( function(element, index) {
+			
+			renderPin(element);
+			renderCard(element);
+	});
+
+	
+
+
 })
 
 
